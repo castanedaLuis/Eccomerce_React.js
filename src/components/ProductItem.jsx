cda8ea25f0c4e191fef90fb7c6728042fec5fc6a
castanedaLuis <castanedaLuis@github.com>
2022-12-17 16:28:03 -0600

    refactorizanbdo coomportaminedo

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,15 +2,25 @@ import React, { useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '../context/AppContext.js'
 import IconAddToCart from '@icons/bt_add_to_cart.svg';
+import iconBtAddedCart from "@icons/bt_added_to_cart.svg"
 
 const ProductItem = (props) => {
-	const {product} = props;
+	const { product } = props;
 
-	const { addToCart } = useContext(AppContext);
+	//Las importaciones
+	const { addToCart, removeFromCart, state } = useContext(AppContext);
 
-	const handleClick = item => {
-		addToCart(item);
-	}
+	// const handleClick = item => {
+	// 	addToCart(item);
+	// }
+
+	//Funcion para agregar/eliminar al carrito
+	const handleClick = (item) => {
+		itsProductAdded() ? removeFromCart(item) : addToCart(item);
+	};
+
+	//Funcion para que busque en el contexto si existe el producto en el carrito 
+	const itsProductAdded = () => state.cart.some((item) => item.id === product.id) ? true : false;
 
 	return (
 		<div className="ProductItem">
@@ -21,7 +31,11 @@ const ProductItem = (props) => {
 					<p>{product.title}</p>
 				</div>
 				<figure className='icon' onClick={() => handleClick(product)} >
-					<img src={IconAddToCart} alt="" />
+					{itsProductAdded() ? (
+						<img src={iconBtAddedCart} alt="" />
+					) : (
+						<img src={IconAddToCart} alt="" />
+					)}
 				</figure>
 			</div>
 		</div>
